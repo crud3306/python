483cf6764a837f8a5b948ff244df88de9035dfaf
qianming <crud3306@163.com>
2018-08-25 07:25:42 +0800

    t

--- base/algorithm/sort.py
+++ base/algorithm/sort.py
@@ -86,6 +86,19 @@ def insert_sort(lists):
         lists[j+1] = min
     return lists
 
+def insert_sort(lists):
+    count = len(lists)
+    for i in range(1, count):
+        key = lists[i]
+        j = i - 1
+        while j >= 0:
+            if lists[j] > key:
+                lists[j + 1] = lists[j]
+                lists[j] = key
+            j -= 1
+    return lists
+
+
 
 
 
